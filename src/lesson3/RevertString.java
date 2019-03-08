7a7f7a7aea654c802a5a1b5064afee46b46a31e1
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-03-08 15:47:44 +0500

    HW 3

---
+++ src/lesson3/RevertString.java
@@ -0,0 +1,58 @@
+package lesson3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class RevertString {
+    public static void main(String[] args) throws IOException {
+        revertStack();
+//        revertMyDec();
+
+    }
+
+    public static void revertStack () throws IOException {
+        System.out.println("Enter string");
+        String flag = "";
+        while (flag != "exit") {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String in = bufferedReader.readLine();
+            Stack<String> stack = new Stack<>();
+            String[] arr = in.split("");
+            for (int i = 0; i < arr.length; i++) {
+                stack.push(arr[i]);
+            }
+            if (in.equals("exit")) {
+                flag = in;
+                break;
+            }
+            while (!stack.empty()) {
+                System.out.print(stack.pop());
+            }
+            System.out.println();
+        }
+    }
+
+//    public static void revertMyDec() throws IOException {
+//        System.out.println("Enter string");
+//        String flag = "";
+//        while (flag != "exit") {
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//            String in = bufferedReader.readLine();
+//            String[] arr = in.split("");
+//            MyDec myDec = new MyDec(arr.length);
+//            for (int i = arr.length - 1; i != -1; i--) {
+//                myDec.addFirst(arr[i]);
+//            }
+//            if (in.equals("exit")) {
+//                flag = in;
+//                break;
+//            }
+//            while (!myDec.isEmpty()) {
+//                System.out.print(myDec.g());
+//            }
+//            System.out.println();
+//        }
+//    }
+}
