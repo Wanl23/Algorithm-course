c26878d070fb07f06b6b7d3d9d0dbd239caac734
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-03-19 15:07:32 +0500

    HW 6

---
+++ src/lesson6/Main.java
@@ -0,0 +1,16 @@
+package lesson6;
+
+public class Main {
+    public static void main(String[] args) {
+        MyTreeMap<Character, Integer> tree = new MyTreeMap<>();
+        tree.put('S', 0);
+        tree.put('E', 0);
+        tree.put('A', 0);
+        tree.put('R', 0);
+        tree.put('C', 0);
+        tree.put('H', 0);
+        tree.put('X', 0);
+        tree.put('M', 0);
+        tree.remove('E');
+    }
+}
