913a4d6c2534f67342cafa125ef27f68583a25f9
IvanLeschinsky <ivan.leschinsky@yandex.ru>
2019-03-19 17:42:40 +0500

    HW 6

--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
         MyTreeMap<Character, Integer> tree = new MyTreeMap<>();
         boolean flag = false;
-        while (!flag) {
-            for (int i = 0; i < 22; i++) {
-                Character key = (char) (i + 65);
-                Integer value = (-100 + (int) (Math.random() * (100 - (-100))));
-                tree.put(key, value);
-            }
-            flag = tree.isBalanced();
-            System.out.println(tree.isBalanced());
+        for (int i = 0; i < 22; i++) {
+            Character key = (char) (i + 65);
+            Integer value = (-100 + (int) (Math.random() * (100 - (-100))));
+            tree.put(key, value);
         }
+        flag = tree.isBalanced();
+        System.out.println(tree.isBalanced());
+
     }
 }
